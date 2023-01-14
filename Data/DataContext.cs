608d6e82f2fa3a0298b61bbea078908387312a5d
Salt4f <47352634+Salt4f@users.noreply.github.com>
2023-01-14 17:32:23 +0100

    Added basic functionality

---
+++ Data/DataContext.cs
@@ -0,0 +1,33 @@
+﻿using GGJWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GGJWeb.Data
+{
+    public class DataContext : DbContext
+    {
+        readonly IConfiguration configuration;
+
+        public DataContext(IConfiguration config)
+        {
+            configuration = config;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options
+                .UseNpgsql(configuration.GetConnectionString("DefaultConnection"))
+                .UseSnakeCaseNamingConvention();
+        }
+
+        public DbSet<Post>? Posts { get; set; }
+        public DbSet<PostInfo>? PostInfos { get; set; }
+        
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Post>()
+                .HasOne(p => p.PostInfo);
+        }
+
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,9 +1,20 @@
+using GGJWeb.Data;
+using GGJWeb.Utils;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<DataContext>();
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var context = scope.ServiceProvider.GetRequiredService<DataContext>();
+    bool created = await context.Database.EnsureCreatedAsync();
+    //if (created) await DBUtils.InitDB(context);
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
